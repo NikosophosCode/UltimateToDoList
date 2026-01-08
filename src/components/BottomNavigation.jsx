be3ddfa9b064389c762a99883093e00b0fc5627a
Nicolas Acuña <niconiampira@hotmail.com>
2026-01-07 19:58:45 -0500

    feat: update BottomNavigation to replace 'tasks' tab with 'profile' tab and adjust icon

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -14,7 +14,7 @@ function BottomNavigation({ activeTab = 'home', onTabChange }) {
 
   const tabs = [
     { id: 'home', icon: 'home' },
-    { id: 'tasks', icon: 'tasks' },
+    { id: 'profile', icon: 'profile' },
     { id: 'calendar', icon: 'calendar' },
     { id: 'analytics', icon: 'analytics' },
     { id: 'settings', icon: 'settings' },
@@ -28,11 +28,10 @@ function BottomNavigation({ activeTab = 'home', onTabChange }) {
             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
           </svg>
         );
-      case 'tasks':
+      case 'profile':
         return (
           <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-            <path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd" />
+            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
           </svg>
         );
       case 'calendar':
