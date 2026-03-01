8eb7d55d8d41f81273c6c0552650f680320ea485
Nicolas Acuña <niconiampira@hotmail.com>
2026-02-28 20:21:18 -0500

    feat(auth): wrap application in GoogleOAuthProvider for OAuth integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { OAUTH_CONFIG } from './config/constants'
 import './index.css'
 import App from './App.jsx'
 
+const googleClientId = OAUTH_CONFIG.GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App className="container mx-auto p-4" />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <App className="container mx-auto p-4" />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
