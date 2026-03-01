8d088b77f7e711bc0c6672d6957bba168db93886
Nicolas Acuña <niconiampira@hotmail.com>
2026-02-28 20:20:54 -0500

    feat(profile): create index file to export profile management components

---
+++ src/components/profile/index.js
@@ -0,0 +1,5 @@
+export { default as AvatarUpload } from './AvatarUpload';
+export { default as ChangePasswordForm } from './ChangePasswordForm';
+export { default as ProfileInfo } from './ProfileInfo';
+export { default as LinkedAccounts } from './LinkedAccounts';
+export { default as DeleteAccount } from './DeleteAccount';
